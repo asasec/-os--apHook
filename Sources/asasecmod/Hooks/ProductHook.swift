bb12ad4981ac2d0acb9e62c08edc8a8f8deb9952
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-20 23:44:57 +0300

    Create ProductHook.swift

---
+++ Sources/asasecmod/Hooks/ProductHook.swift
@@ -0,0 +1,21 @@
+import Jinx
+import StoreKit
+
+struct ProductHook: Hook {
+    typealias T = @convention(c) (AnyObject, Selector) -> NSDecimalNumber
+
+    let cls: AnyClass? = SKProduct.self
+    let sel: Selector = #selector(getter: SKProduct.price)
+    
+    var replace: T {
+        return { selfObj, sel in
+            // Eğer özellik açıksa fiyatı 0.01 yap
+            if Preferences.isZeroPointOnePriceEnabled {
+                return 0.01
+            }
+            // Kapalıysa orijinal fonksiyonu çalıştırıp normal fiyatı döndür
+            let originalMethod = unsafeBitCast(getOriginal(), to: T.self)
+            return originalMethod(selfObj, sel)
+        }
+    }
+}
