d805a0d0e4f2dba886efd7f3381055851eaf2d7d
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-23 17:49:25 +0300

    Delete Sources/asasecmod/KittyMemory directory

--- Sources/asasecmod/KittyMemory/MemoryPatchManager.h
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface MemoryPatchManager : NSObject
-+ (BOOL)writeMemoryAtOffset:(uint64_t)offset withBytes:(NSArray<NSNumber *> *)bytes forLibrary:(NSString *)libraryName;
-@end
