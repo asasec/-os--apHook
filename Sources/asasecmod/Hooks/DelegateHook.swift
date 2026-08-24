8f7839390c918788edfb068ee8bd4e9c73588e06
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-24 16:54:39 +0300

    Update DelegateHook.swift

--- Sources/asasecmod/Hooks/DelegateHook.swift
+++ Sources/asasecmod/Hooks/DelegateHook.swift
@@ -2,13 +2,19 @@ import Jinx
 import StoreKit
 
 struct DelegateHook: Hook {
-    typealias T = @convention(c) (AnyObject, Selector, SKProductsRequestDelegate) -> Void
+    typealias T = @convention(c) (AnyObject, Selector, SKProductsRequestDelegate?) -> Void
 
     let cls: AnyClass? = SKProductsRequest.self
     let sel: Selector = sel_registerName("setDelegate:")
+    
     let replace: T = { obj, sel, delegate in
         let tella: AsasecDelegate = .shared
-        tella.delegates.append(delegate)
+        if let unwrappedDelegate = delegate {
+            // Bellek sızıntısını önlemek için aynı delegate tekrar ekleniyorsa ekleme
+            if !tella.delegates.contains(where: { $0 === unwrappedDelegate }) {
+                tella.delegates.append(unwrappedDelegate)
+            }
+        }
         orig(obj, sel, tella)
     }
 }
