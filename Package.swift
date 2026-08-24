75cb890a64dd4c1a9358a713edad0252f2eeaac5
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-25 01:35:26 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,3 +1,21 @@
+// swift-tools-version:5.8
+
+import Darwin.POSIX
+import PackageDescription
+
+let theosPath: String = .init(cString: getenv("HOME")) + "/theos"
+let minFirmware: String = "12.2"
+
+let swiftFlags: [String] = [
+    "-F\(theosPath)/vendor/lib",
+    "-F\(theosPath)/lib",
+    "-I\(theosPath)/vendor/include",
+    "-I\(theosPath)/include",
+    "-target", "arm64-apple-ios\(minFirmware)",
+    "-sdk", "\(theosPath)/sdks/iPhoneOS16.0.sdk",
+    "-resource-dir", "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift"
+]
+
 let package: Package = .init(
     name: "Asasecİap",
     platforms: [.iOS(minFirmware)],
@@ -8,13 +26,15 @@ let package: Package = .init(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Paisseon/Jinx.git", branch: "development")
+        .package(url: "https://github.com/Paisseon/Jinx.git", branch: "development"),
+        .package(url: "https://github.com/FLEXTool/FLEX.git", branch: "master")
     ],
     targets: [
         .target(
             name: "Asasecİap",
             dependencies: [
-                .product(name: "Jinx", package: "Jinx")
+                .product(name: "Jinx", package: "Jinx"),
+                .product(name: "FLEX", package: "FLEX")
             ],
             path: "Sources/asasecmod",
             sources: [
